265e131101471d8c43e4893fb13d5280333be9fe
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-08-26 13:04:11 -0400

    char counts created for inputs

--- javascript.js
+++ javascript.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
 
   //listener for text input field
-  $('#first_input').keyup(function(){charCount(this)});
 
-  var charCount = function(e){
-    console.log(e);
-    var charNum = e.value.length;
-      console.log("e is " + (typeof e));
+  $('#first_input').keyup(function(){inputLimitDisplay(this, 32)});
+  $('#second_input').keyup(function(){inputLimitDisplay(this, 140)});
+
+
+  var inputLimitDisplay = function(e, maxChar){
+    e = $(e);
+    var charNum = e.val().length;
+    var charRemain = maxChar - charNum;
+    var spanID = e.attr("id") + "limit";
     if (charNum <=0){
       //if count previously exists, remove
-      $(e.siblings().first()).hide();
+      $("#" + spanID).remove();
     } else {
       // charNum
-    }
+      if($("#" + spanID).length == 0) {
+        //it doesn't exist
+        e.parent().append("<span id='" + spanID + "'>" + charRemain + " characters remaining</span>");
+      }
+      else {
+        $("#" + spanID).text(charRemain + " characters remaining");
+      };
+    };
   };
 
-  var
 });
